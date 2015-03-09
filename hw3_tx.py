d8a0bb00a39eeb6bd874761242dbfde5afc301cf
ACollectionOfAtoms <hernandezdavid88@utexas.edu>
2015-03-08 20:45:59 -0500

    Added logging in functionality

--- hw3_tx.py
+++ hw3_tx.py
@@ -1,13 +1,28 @@
-import cx_Oracle   
+import cx_Oracle
+import sys
+con = cx_Oracle.connect('dah3227_om/Oradah3227@//net6.cs.utexas.edu:1521/orcl')
+cur = con.cursor()
 
-con = cx_Oracle.connect('dah3227_ap/Oradah3227@//net6.cs.utexas.edu:1521/orcl')
+class User:
+    def __init__(self,login,password):
+        self.login = login
+        self.password = password
+        
+    def connect(self):
+        cur.execute('SELECT CUSTOMER_LOGIN, CUSTOMER_PASSWORD FROM CUSTOMERS')
+        info = cur.fetchall()
+        if (self.login,self.password) in info:
+            print 'Login Succesful'
+        else:
+            print 'Login Failed'
 
-print con.version
-
-
-
-
-
-
-
+def main():
+    print "Welcome to Pandorify-Shark!"
+    username = raw_input("Username: ")
+    password = raw_input("Password: ")
+    u = User(username,password)
+    u.connect()
+main()
+cur.close()
+con.close()
 
